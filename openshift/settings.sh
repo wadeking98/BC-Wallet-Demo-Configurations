4c6c3d5ca0759f639cd657580c84143a42df9990
wadeking98 <wkingnumber2@gmail.com>
2022-05-16 10:51:47 -0700

    initial commit
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

---
+++ openshift/settings.sh
@@ -0,0 +1,9 @@
+export PROJECT_NAMESPACE="a99fd4"
+export GIT_URI="https://github.com/bcgov/BC-Wallet-Demo.git"
+export GIT_REF="main"
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides=""
+export ignore_templates=""
